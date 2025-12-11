e42ba46eeb995fd29ba4a7fa84d38ee6b29cf5d6
Test User <test@example.com>
2025-12-10 22:46:54 -0700

    log relative position in rotations

--- src/main/java/frc/robot/subsystems/drive/DriveModuleSpark.java
+++ src/main/java/frc/robot/subsystems/drive/DriveModuleSpark.java
@@ -39,6 +39,8 @@ public class DriveModuleSpark extends SubsystemBase {
     Logger.recordOutput(
         "Drive/Module" + id + "/relativePosMeters", getRelativePosition().in(Units.Meter));
     Logger.recordOutput(
+        "Drive/Module" + id + "/relativePosRotations", encoder.getPosition());
+    Logger.recordOutput(
         "Drive/Module" + id + "/velMetersPerSecond", getVel().in(Units.MetersPerSecond));
     Logger.recordOutput(
         "Drive/Module" + id + "/DesiredVelMetersPerSecond", desiredVel.in(Units.MetersPerSecond));
