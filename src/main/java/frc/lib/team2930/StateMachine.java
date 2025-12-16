793cef56f777a7b1161e0643ff01bb479cd3a4ec
codecreator3 <timellis337@gmail.com>
2025-12-16 15:57:11 -0800

    import state machine

---
+++ src/main/java/frc/lib/team2930/StateMachine.java
@@ -0,0 +1,313 @@
+package frc.lib.team2930;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class StateMachine {
+  public enum Status {
+    Running,
+    Done,
+    Stopped
+  }
+
+  @Retention(RUNTIME)
+  @Target(METHOD)
+  public @interface TimerEvent {
+    double after();
+  }
+
+  @FunctionalInterface
+  public interface StateHandler {
+    StateHandler advance();
+  }
+
+  public interface StateHandlerWithName extends StateHandler {
+    String getName();
+  }
+
+  @FunctionalInterface
+  public interface ResumeStateHandler {
+    StateHandler advance(StateMachine subStateMachine);
+  }
+
+  @FunctionalInterface
+  public interface ResumeStateHandlerFromCommand {
+    StateHandler advance(Command command);
+  }
+
+  static class EventState {
+    EventState next;
+    Method md;
+    double trigger;
+
+    EventState(Method md, TimerEvent anno) {
+      this.md = md;
+      this.trigger = anno.after();
+    }
+  }
+
+  private static final LoggerGroup logGroup = LoggerGroup.build("StateMachine");
+
+  private final String name;
+  private final LoggerEntry.Text logName;
+  private Status status;
+  private StateHandler currentState;
+  private double startTime;
+  private double startTimeOfState;
+  private EventState nextEvent;
+  private StateHandler interruptedState = () -> setDone();
+  private ArrayList<Command> commands = new ArrayList<Command>(0);
+
+  protected StateMachine(String name) {
+    this.name = name;
+    logName = logGroup.buildString(name);
+
+    var events = new ArrayList<EventState>();
+
+    for (Method md : this.getClass().getDeclaredMethods()) {
+      var anno = md.getAnnotation(TimerEvent.class);
+      if (anno != null) {
+        events.add(new EventState(md, anno));
+      }
+    }
+
+    // Sort in increasing time.
+    events.sort(Comparator.comparing((v) -> v.trigger));
+
+    // Chain them in time order, to make it easier to process them.
+    EventState previous = null;
+
+    for (var event : events) {
+      if (previous == null) {
+        nextEvent = event;
+      } else {
+        previous.next = event;
+      }
+
+      previous = event;
+    }
+  }
+
+  protected void setInitialState(StateHandler initialState) {
+    status = Status.Running;
+    startTime = Timer.getFPGATimestamp();
+    setNextState(initialState);
+  }
+
+  protected StateHandlerWithName stateWithName(String name, StateHandler handler) {
+    return new StateHandlerWithName() {
+      @Override
+      public String getName() {
+        return name;
+      }
+
+      @Override
+      public StateHandler advance() {
+        return handler.advance();
+      }
+    };
+  }
+
+  private void setNextState(StateHandler nextState) {
+    currentState = nextState;
+    startTimeOfState = Timer.getFPGATimestamp();
+
+    if (currentState instanceof StateHandlerWithName stateWithName) {
+      logName.info(stateWithName.getName());
+    } else {
+      logName.info(nextState.getClass().getName());
+    }
+  }
+
+  protected void setInterruptedState(StateHandler interruptedState) {
+    this.interruptedState = interruptedState;
+  }
+
+  public Command asCommand() {
+    return asCommand(false);
+  }
+
+  public Command asCommand(boolean runsWhenDisabled) {
+    return new Command() {
+      @Override
+      public void initialize() {
+        StateMachine.this.startTime = Timer.getFPGATimestamp();
+      }
+
+      @Override
+      public void execute() {
+        advance();
+      }
+
+      public boolean isFinished() {
+        return !isRunning();
+      }
+
+      @Override
+      public void end(boolean interrupted) {
+        for (int i = 0; i < commands.size(); i++) {
+          commands.get(i).cancel();
+        }
+
+        if (interrupted) {
+          setNextState(interruptedState);
+          advance();
+        }
+      }
+
+      @Override
+      public boolean runsWhenDisabled() {
+        return runsWhenDisabled;
+      }
+    };
+  }
+
+  public void advance() {
+    while (isRunning()) {
+      processEvents();
+
+      var nextState = currentState.advance();
+      if (nextState == null) break;
+
+      setNextState(nextState);
+    }
+  }
+
+  public void logCurrentState(String name) {
+    logName.info(currentState.getClass().getName());
+  }
+
+  private void processEvents() {
+    var elapsedTime = timeFromStart();
+
+    for (var event = nextEvent; event != null; event = event.next) {
+      if (event.trigger > elapsedTime) {
+        nextEvent = event;
+        break;
+      }
+
+      try {
+        var nextState = (StateHandler) event.md.invoke(this);
+        if (nextState != null) {
+          setNextState(nextState);
+        }
+      } catch (Throwable e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public double timeFromStart() {
+    return Timer.getFPGATimestamp() - startTime;
+  }
+
+  public double timeFromStartOfState() {
+    return Timer.getFPGATimestamp() - startTimeOfState;
+  }
+
+  public boolean stateRunningLongerThan(double time) {
+    return timeFromStartOfState() > time;
+  }
+
+  public boolean isRunning() {
+    return status == Status.Running;
+  }
+
+  public boolean wasStopped() {
+    return status == Status.Stopped;
+  }
+
+  protected StateHandler setDone() {
+    status = Status.Done;
+    logName.info("<done>");
+    return null;
+  }
+
+  protected StateHandler setStopped() {
+    status = Status.Stopped;
+    logName.info("<stopped>");
+    return null;
+  }
+
+  protected StateHandler suspendForSubStateMachine(
+      StateMachine subStateMachine, ResumeStateHandler handler) {
+    return stateWithName(
+        "waitFor" + subStateMachine.name,
+        () -> {
+          subStateMachine.advance();
+          if (subStateMachine.isRunning()) return null;
+
+          return handler.advance(subStateMachine);
+        });
+  }
+
+  protected Command spawnStateMachineAsCommand(
+      StateMachine subStateMachine, ResumeStateHandlerFromCommand handler) {
+    return spawnStateMachineAsCommand(subStateMachine, handler, false);
+  }
+
+  protected Command spawnStateMachineAsCommand(
+      StateMachine subStateMachine,
+      ResumeStateHandlerFromCommand handler,
+      boolean runsWhenDiabled) {
+    Command command = subStateMachine.asCommand(runsWhenDiabled);
+    spawnCommand(command, handler);
+    return command;
+  }
+
+  protected StateHandler suspendForCommand(Command command, ResumeStateHandlerFromCommand handler) {
+    AtomicBoolean done = new AtomicBoolean();
+
+    spawnCommand(
+        command,
+        (c) -> {
+          done.set(true);
+          return null;
+        });
+
+    var name = command.getName();
+    return stateWithName(
+        "waitForCmd " + name,
+        () -> {
+          if (!done.get()) return null;
+
+          return handler.advance(command);
+        });
+  }
+
+  protected Command spawnCommand(Command command, ResumeStateHandlerFromCommand handler) {
+    var sequence =
+        command.andThen(
+            new InstantCommand(
+                () -> {
+                  if (!isRunning()) return;
+
+                  var nextState = handler.advance(command);
+                  if (nextState != null) {
+                    setNextState(nextState);
+                  }
+                }) {
+              @Override
+              public boolean runsWhenDisabled() {
+                return true;
+              }
+            });
+
+    commands.add(sequence);
+
+    sequence.setName("substate_" + command.getName());
+    sequence.schedule();
+
+    return sequence;
+  }
+}
