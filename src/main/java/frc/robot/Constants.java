8d06b4c493b9d948d0f938445e0ed42b0dc18589
mvande <matvandeberg@gmail.com>
2026-01-09 14:00:26 -0700

    create shooter constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -176,6 +176,24 @@ public final class Constants {
     }
   }
 
+  public class ShooterConstants {
+    public static final String ROOT_TABLE = "Intake";
+    public static final double GEAR_RATIO = 1; // TODO: determine (pulley / motor)
+    public static final boolean INVERT = false; // TODO: determine
+    public static final double MOI = 0.0001;
+
+    public static final SparkFlexConfig MOTOR_CONFIG() {
+      SparkFlexConfig config = new SparkFlexConfig();
+      config.absoluteEncoder.inverted(INVERT);
+      config.inverted(INVERT);
+      config.idleMode(IdleMode.kBrake);
+      config.smartCurrentLimit(40);
+      config.encoder.positionConversionFactor(GEAR_RATIO);
+      config.encoder.velocityConversionFactor(GEAR_RATIO);
+      return config;
+    }
+  }
+
   public class VisionConstants {
     // AprilTag layout
     public static AprilTagFieldLayout aprilTagLayout =
